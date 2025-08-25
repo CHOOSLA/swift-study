2aa58e2fecb7ea14a55f7bc75e8f5481ff8de63f
Choosla <cnckddn0146@gmail.com>
2025-08-25 17:28:02 +0900

    [auto] 2025-08-25 17:28:02 +0900 (repo: Swift, branch: main, base: 8fdae91, author: Choosla) files: total 2 (A:0 M:2 D:0 R:0 C:0) | lines: +21 -5 changes:  - Practive06.playground/Contents.swift  - Practive06.playground/playground.xcworkspace/xcuserdata/admin.xcuserdatad/UserInterfaceState.xcuserstate

--- Practive06.playground/Contents.swift
+++ Practive06.playground/Contents.swift
@@ -2,15 +2,31 @@ import UIKit
 
 class Student{
   // [과목 : 점수]인 딕셔너리
+  let name: String?
   var subjectScore: [String:Int]? = [:]
   
-  func addSubjectScore(_ subject: String , _ score: Int){
-    subjectScore?[subject] = score
+  func addSubjectScore(_ subject: String? , _ score: Int?){
+    if subject == nil {return}
+    subjectScore?[subject!] = score
   }
-  func getSubjectScore() -> [String:Int]?{
-    return self.subjectScore
+  
+  init(_ name: String?) {
+    self.name = name
   }
+  
 }
 
-let student1 = Student()
+func getSubjectScore(student: Student?) -> [String:Int]?{
+  return student?.subjectScore
+}
+
+let student1 = Student("Alice")
+student1.addSubjectScore("Math", 90)
 print("테스트 1: 학생과 과목 점수 존재")
+print("학생: \(student1.name ?? "Unknown") , Math 점수: \(student1.subjectScore?["Math"] ?? -1)") // ??로 nil 처리
+
+let student2 = Student(nil)
+print("학생: \(student2.name ?? "학생이 nil인 경우"), Math 점수: \(student2.subjectScore?["Math"] ?? -1)")
+
+let student3 = Student("Bob")
+print("학생: \(student3.name ?? "Unknown") , Math 점수: \(student3.subjectScore?["Math"] ?? -1)") // ??로 nil 처리
