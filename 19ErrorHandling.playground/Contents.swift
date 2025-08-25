ad69843e3b2a2b1e88abfbe69ac5efc11f10b4ae
Choosla <cnckddn0146@gmail.com>
2025-08-25 16:27:42 +0900

    [auto] 2025-08-25 16:27:42 +0900 (repo: Swift, branch: main, base: e193f7d, author: Choosla) files: total 2 (A:0 M:2 D:0 R:0 C:0) | lines: +42 -3 changes:  - 19ErrorHandling.playground/Contents.swift  - 19ErrorHandling.playground/playground.xcworkspace/xcuserdata/admin.xcuserdatad/UserInterfaceState.xcuserstate

--- 19ErrorHandling.playground/Contents.swift
+++ 19ErrorHandling.playground/Contents.swift
@@ -10,10 +10,49 @@ let b = 0
 // a/b
 // print("코드 수행1")
 
+//do{
+//  print("코드 수행1")
+//  a/b
+//  print("코드 수행2")
+//}catch{
+//  print("수행문이 에러가 발생")
+//}
+
+enum MyError: Error{
+  case somethingWentWrong
+  case somethingWentWrong2
+  case somethingWentWrong3
+}
+
+func riskyFunction() throws -> Int{
+  print("실행하다가 문제 발생!")
+  throw MyError.somethingWentWrong
+  print("이 코드는 실행되지 않는다.")
+}
+
 do{
   print("코드 수행1")
-  a/b
+  try riskyFunction()
   print("코드 수행2")
-}catch{
-  print("수행문이 에러가 발생")
+} catch {
+  print("수행문이 예외를 발생시킴")
+}
+
+// 구조체
+struct NetworkError: Error{
+  let code: Int
+  let message: String
+}
+
+do{
+  // 에러 직접 던지기 (상태 코드와 메시지를 함께 포함)
+  throw NetworkError(code: 404, message: "Not Found")
+} catch{
+//  print("구조체 에러 잡힙 : \(error)")
+//  print("구조체 에러 잡힙 : \(error.code) \(error.message)") // 이렇게 바로 못 쓰고 형변환 해야 한다.
+  if let networkError = error as? NetworkError {
+    print("구조체 에러 잡힙 : \(networkError.code) \(networkError.message)")
+  } else {
+    print("알 수 없는 에러 \(error)")
+  }
 }
